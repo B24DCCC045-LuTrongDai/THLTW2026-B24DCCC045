0a64340705a9e527f69239efd7fc6a4979b332e3
lutrongdai <lutrongd@gmail.com>
2026-01-20 15:32:01 +0700

    nopbai

--- config/routes.ts
+++ config/routes.ts
@@ -1,4 +1,6 @@
-﻿export default [
+﻿import path from "path";
+
+export default [
 	{
 		path: '/user',
 		layout: false,
@@ -41,6 +43,12 @@
 		name: 'TodoList',
 		icon: 'OrderedListOutlined',
 		component: './TodoList',
+	},
+	{
+		path: '/quan-ly-san-pham',
+		name: 'QuanLySanPham',
+		icon: 'AppstoreOutlined',
+		component: './QuanLySanPham',
 	},
 
 	// DANH MUC HE THONG
